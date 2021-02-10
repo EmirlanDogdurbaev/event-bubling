19a471f7060e678c4d16e12b4eb75b7e1330051f
EmirlanDogdurbaev <jigit38@gmail.com>
2021-02-10 15:59:20 +0300

    finish lesson

--- index.js
+++ index.js
@@ -107,3 +107,34 @@ parent.addEventListener('mouseover', function(event) {
 parent.addEventListener('mouseout', function(event) {
     console.log('mouseout - ' + this.className);
 });
+
+
+
+// Timer
+let timerID;
+let button = document.querySelector('button#hello');
+let cancel = document.querySelector('button#cancel');
+button.addEventListener('click', function() {
+   timerID  = setTimeout(sayHello, 5000);
+});
+cancel.addEventListener('click', function() {
+   clearTimeout(timerID);
+});
+
+// Iterval
+let intervalID;
+let number = 10;
+let countdown = document.querySelector('#countdown');
+let start = document.querySelector('#start');
+start.addEventListener('click', function() {
+    clearInterval(intervalID);
+    number = 10;
+    
+    intervalID = setInterval(function() {
+        countdown.innerText = number;
+        number--;
+        if (number == -1) {
+            clearInterval(intervalID);
+        }
+    }, 1000);
+});
